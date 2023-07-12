28331d8fb80da25812aac634814b606e7d2adc3e
Mur-Dev <murraypoulson.com>
2023-07-11 18:04:03 -0600

    Added Logic for Containment and prisoner

---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+let $Username = 'test'
+
+
+console.log(isPrisoner())
+
+function containmentEntry(){
+    $Test = Array.from(document.querySelectorAll('#EntryForm input')).reduce((acc, input) => ({...acc, [input.id]: input.value }), {});
+    console.log($Test);
+    //alert('submitted');
+    if ($Username == $Test.Username && $Username == $Test.Password){
+        console.log('Access Granted');
+        
+        let $prisonerFlag = 'true'
+        sessionStorage['prisonerFlag'] = $prisonerFlag    
+        
+        document.getElementById('MessageWrapper').innerHTML = `<p>Access Granted: are you a prisoner? ${isPrisoner()}</p>`;
+        //window.open('./prisoner.html', '_self')
+    }
+    else{
+        console.log('Access Denied');
+        document.getElementById('MessageWrapper').innerHTML = `<p>Access Denied: are you a prisoner? ${isPrisoner()}</p>`;
+    }
+    //console.log($Test.Username)
+    console.log(isPrisoner());
+    imprisonment();
+}
+
+function isPrisoner(){
+    let $readValue = sessionStorage['prisonerFlag'];
+    if($readValue == 'true'){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+function imprisonment(){
+    if(isPrisoner()){
+        window.open('./prisoner.html', '_self');
+    }
+}
+
+imprisonment();
