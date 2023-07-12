ba84dbc9af02d84f290ec5828b6e36f4d4d9b682
Mur-Dev <murraypoulson.com>
2023-07-11 18:21:08 -0600

    Added HTML Class Links and styled in css. Updated html page titles.

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
+//Puzzle Answers
 let $Username = 'test'
 
 
 console.log(isPrisoner())
 
+
+//Containment Form Function
 function containmentEntry(){
     $Test = Array.from(document.querySelectorAll('#EntryForm input')).reduce((acc, input) => ({...acc, [input.id]: input.value }), {});
     console.log($Test);
@@ -41,4 +44,6 @@ function imprisonment(){
     }
 }
 
+//main.js will need to be added to each of the files in the website to make sure users cannot escape the prison
+//in its current form the prison sentence ends when you terminate the browser session, could look into other more permanent options
 imprisonment();
