525a65211c024d1de2745d82fb8e068bbe246c17
Ryan McBride <ryan.mcbride@ciphertracelabs.com>
2023-07-18 21:29:42 -0600

    Hey

--- scripts/main.js
+++ scripts/main.js
@@ -12,12 +12,11 @@ function containmentEntry(){
     //alert('submitted');
     if ($Username == $Test.Username && $Username == $Test.Password){
         console.log('Access Granted');
-        
         let $prisonerFlag = 'true'
         sessionStorage['prisonerFlag'] = $prisonerFlag    
         
         document.getElementById('MessageWrapper').innerHTML = `<p>Access Granted: are you a prisoner? ${isPrisoner()}</p>`;
-        //window.open('./prisoner.html', '_self')
+        window.open('./prisoner.html', '_self')
     }
     else{
         console.log('Access Denied');
@@ -47,3 +46,28 @@ function imprisonment(){
 //main.js will need to be added to each of the files in the website to make sure users cannot escape the prison
 //in its current form the prison sentence ends when you terminate the browser session, could look into other more permanent options
 imprisonment();
+
+//cookies
+
+function cookieCreate(cookieName,value,hours){
+    const date = new Date();
+    date.setTime(date.getTime() + (hours*60*60*1000));
+    let expires = "expires=" + date.toUTCString();
+    document.cookie = cookieName + "=" + value + ";" + expires + ";path=/";
+}
+
+function cookieGet(cookieName) {
+    let cookie = cookieName + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(cookieName) === 0) {
+        return c.substring(cookieName.length + 1, c.length); // Add +1 to skip the "=" character
+      }
+    }
+    return "";
+  }
