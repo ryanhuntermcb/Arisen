f8e1e33cd8134d606e73e0392975bc22f063f30c
Ryan McBride <ryan.mcbride@ciphertracelabs.com>
2023-09-25 20:53:09 -0600

    Start and Prisoner logic changes

---
+++ scripts/start.js
@@ -0,0 +1,31 @@
+//Sets user to no longer get the intro script
+function isOld() {
+    let $oldSession = 'false'
+    sessionStorage['newSession'] = $oldSession
+}
+
+script = ["I can never really know.","I'm stuck here as usual.","I guess I was just born wrong.\
+            ","Well, I hope you enjoy your stay.<br> if you click around...\
+            ","You might just find something interesting!","You might even find me!"]
+
+//Below is run by the start page
+function startScript() {
+
+    let i = 0;
+
+    const intervalId = setInterval(function () {
+        if (i < script.length) {
+            console.log(script[i]);
+            document.getElementById("start").innerHTML = script[i];
+            i++;
+        } else {
+            clearInterval(intervalId); // Stop the interval when all elements are processed
+            isOld()
+            window.open('./home.html', '_self');
+        }
+    }, 4000);
+
+    
+}
+
+startScript()
