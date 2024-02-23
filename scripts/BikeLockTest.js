34afcc7d1a689c1dfd42f5e185624eb6b91c3013
Mur-Dev <139187813+Mur-Dev@users.noreply.github.com>
2024-02-22 20:56:02 -0700

    Finished function to create one wheel of the bike lock test.

--- scripts/BikeLockTest.js
+++ scripts/BikeLockTest.js
@@ -1,17 +1,68 @@
+function CreateOneWheelLetterOption(inputArray, inputTargetContainerId, inputSelectListName) {
+    let inputArrayLength
+    let TargetContainer = document.getElementById(inputTargetContainerId)
 
-document.addEventListener("DOMContentLoaded", () => {
-    
-    let inputArray = ['1','2','3']
-
-    inputArray.forEach((i)=>{
-        console.log('x')
+    if (Array.isArray(inputArray)) {
+        inputArrayLength = inputArray.length
+    }
+    else {
+        inputArrayLength = 1
     }
 
-    )
-    let optionInput = "xyz"
-    let optionTemplate = `<option value=${optionInput}>${optionInput}</option>`
+    TargetContainer.innerHTML += `<select name=${inputSelectListName} id=${inputSelectListName} class="dropbtn "></select>`
 
-    document.getElementById('bun').innerHTML += optionTemplate
+    for (let i = 0; i < inputArrayLength; i++) {
+        //console.log(`${i} and ${inputArrayLength}`)
+
+        let firstElement = ''
+
+        if (i == 0) {
+            firstElement = 'Select'
+        }
+
+        let optionTemplate = `<option value=${inputArray[i]}>${inputArray[i]}</option>`
+        
+        document.getElementById(inputSelectListName).innerHTML += optionTemplate
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    //let optionTemplate = `<option value=${optionInput}>${optionInput}</option>`
+    CreateOneWheelLetterOption([1,2,3], 'PuzzleContainer', 'Letter1')
+    CreateOneWheelLetterOption([4,5,6], 'PuzzleContainer', 'Letter2')
+
+/*
+    let inputArray = ['1', '2', '3']
+    let inputTargetContainerId = 'PuzzleContainer'
+    let inputSelectListName = 'TestList'
+
+    let TargetContainer = document.getElementById(inputTargetContainerId)
+    TargetContainer.innerHTML += `<select name=${inputSelectListName} id=${inputSelectListName} class="dropbtn "></select>`
+
+    console.log(inputArray.length)
+    for (let i = 0; i < inputArray.length; i++) {
+        console.log(`${i} and ${inputArray.length}`)
+
+        let firstElement = ''
+
+        if (i == 0) {
+            firstElement = 'Select'
+        }
+        let optionTemplate = `<option value=${inputArray[i]}>${inputArray[i]}</option>`
+        document.getElementById(inputSelectListName).innerHTML += optionTemplate
+    }
+    */
+    /*
+        for (let i = 0; i < inputArray.length ; i++) {
+            let firstElement = (i = 0 ? 'selected' : '')
+            let optionTemplate = `<option value=${optionInput} ${firstElement}>${optionInput}</option>`
+            document.getElementById('bun').innerHTML += optionTemplate
+            console.log('test')
+        }
+    */
+
+    //document.getElementById('bun').innerHTML += optionTemplate
 
     /*
     let ButtonOne = document.getElementById("TextButtonOne");
@@ -27,33 +78,33 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     //document.getElementById('TextOne').innerHTML = TestArray.join("");
-/*
-    ButtonOne.addEventListener('click', ()=>{
-        if(!LargeTextBox.classList.contains("MoveTextAnimationDown")){
-            LargeTextBox.classList.add("MoveTextAnimationDown")
-            console.log(1)
-        }
-        else {
-            LargeTextBox.classList.remove("MoveTextAnimationDown")
-            console.log(2)
-        }
-    })
-*/
-/*
-    let Ptext = document.getElementById("Text")
-    Ptext.addEventListener('click', ()=>{
-        if(!Ptext.classList.contains("fadeInUp-animation")){
-            Ptext.classList.add("fadeInUp-animation")
-            console.log(3)
-            Ptext.innerHTML='New'
-            
-        }
-        else {
-            Ptext.classList.remove("fadeInUp-animation")
-            console.log(4)
-        }
-    })
+    /*
+        ButtonOne.addEventListener('click', ()=>{
+            if(!LargeTextBox.classList.contains("MoveTextAnimationDown")){
+                LargeTextBox.classList.add("MoveTextAnimationDown")
+                console.log(1)
+            }
+            else {
+                LargeTextBox.classList.remove("MoveTextAnimationDown")
+                console.log(2)
+            }
+        })
     */
+    /*
+        let Ptext = document.getElementById("Text")
+        Ptext.addEventListener('click', ()=>{
+            if(!Ptext.classList.contains("fadeInUp-animation")){
+                Ptext.classList.add("fadeInUp-animation")
+                console.log(3)
+                Ptext.innerHTML='New'
+                
+            }
+            else {
+                Ptext.classList.remove("fadeInUp-animation")
+                console.log(4)
+            }
+        })
+        */
 })
 
 //Function to add drop downs
