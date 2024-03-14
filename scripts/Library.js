2372251877d6e6020a6435d937eb9c16fd945019
Mur-Dev <139187813+Mur-Dev@users.noreply.github.com>
2024-03-13 20:43:35 -0600

    Added Bike Lock functions to library.js

--- scripts/Library.js
+++ scripts/Library.js
@@ -316,6 +316,125 @@ function areAllPuzzlesComplete() {
     }
 }
 
+//---------------------------------------------------
+//--------------Bike Lock Puzzle---------------------
+//---------------------------------------------------
+function CreateOneWheelLetterOption(inputArray, inputTargetContainerId, inputSelectListName) {
+    let inputArrayLength
+    let TargetContainer = document.getElementById(inputTargetContainerId)
+
+    if (Array.isArray(inputArray)) {
+        inputArrayLength = inputArray.length
+    }
+    else {
+        inputArrayLength = 1
+    }
+
+    TargetContainer.innerHTML += `<select name=${inputSelectListName} id=${inputSelectListName} class="dropbtn "></select>`
+
+    for (let i = 0; i < inputArrayLength; i++) {
+        //console.log(`${i} and ${inputArrayLength}`)
+
+        let firstElement = ''
+
+        if (i == 0) {
+            firstElement = 'Select'
+        }
+
+        let optionTemplate = `<option value=${inputArray[i]}>${inputArray[i]}</option>`
+
+        document.getElementById(inputSelectListName).innerHTML += optionTemplate
+    }
+}
+
+function CreateBikeLockPuzzle(PuzzleSolution, PuzzleDifficulty, PuzzleName, TargetContainerId) {
+    PuzzleSolution = String(PuzzleSolution).toUpperCase()
+    let NumberOfWheels = PuzzleSolution.length
+    let PuzzleSolutionArray = PuzzleSolution.split('')
+    let TargetContainer = document.getElementById(TargetContainerId)
+    TargetContainer.innerHTML = '' //The puzzle should be the only thing inside of the target container
+
+    //Ensure PuzzleDifficulty is between 1 and 10
+    if (!(PuzzleDifficulty > 0)) {
+        PuzzleDifficulty = 1
+    }
+    if (PuzzleDifficulty > 10) {
+        PuzzleDifficulty = 10
+    }
+
+    //console.log(PuzzleSolutionArray) //Answer but split into Array
+
+    for (let i = 0; i < NumberOfWheels; i++) {
+        let start = PuzzleSolutionArray[i]
+        let arrayTest = [start]
+        let i2 = 0
+
+        while (i2 < PuzzleDifficulty) {
+            let rand = randomLetter()
+            if (arrayTest.includes(rand)) {
+                continue
+            }
+            else {
+                //console.log(rand)
+                arrayTest.push(rand)
+                i2++
+                //console.log(randomIndex(5))
+            }
+        }
+
+        //Solution letter index swap
+        let swapToIndex = randomIndex(arrayTest.length)
+
+        let swapFrom = arrayTest[0]
+        arrayTest[0] = arrayTest[swapToIndex]
+        arrayTest[swapToIndex] = swapFrom
+
+        CreateOneWheelLetterOption(arrayTest, TargetContainerId, `${PuzzleName}${i}`)
+
+        //console.log(swapToIndex)
+        //console.log(arrayTest)
+        PuzzleSolutionArray[i] = arrayTest
+
+        //let finish = 
+    }
+
+
+    //console.log(PuzzleSolutionArray)
+    for (let i = 0; i < NumberOfWheels; i++) {
+        //CreateOneWheelLetterOption(PuzzleSolutionArray[i],inputTargetContainerId, inputSelectListName)
+    }
+
+}
+
+function randomLetter() {
+    const uppercaseAsciiStart = 65;
+    const letterIndex = Math.floor(Math.random() * 26);
+    const letter = String.fromCharCode(uppercaseAsciiStart + letterIndex);
+    return letter
+}
+
+function randomIndex(ArrayLength) {
+    return Math.floor(Math.random() * ArrayLength)   //this should be a random spot
+}
+
+function IsPuzzleSolved(PuzzleContainerId, PuzzleSolution) {
+    let PuzzleParent = document.getElementById(PuzzleContainerId).children
+    //console.log(PuzzleParent)
+
+    let currentAnswer = ''
+    for (let i = 0; i < PuzzleParent.length; i++) {
+        currentAnswer += PuzzleParent[i].value
+    }
+
+    if (currentAnswer == PuzzleSolution) {
+        return true
+    }
+    else {
+        return false
+    }
+
+}
+
 
 export {
     containmentEntry, imprisonment, isPrisoner, start, //main //Containment
@@ -325,7 +444,7 @@ export {
     checkContainmentPuzzleEventHandler, hasWonPuzzle, clickABoutPhoto, //GameLog
     
     resetProgress, getProgressTrackerState, incrementProgressTracker, //New Progress Tracker
-    getOverallProgress, areAllPuzzlesComplete //New Progress Tracker
+    getOverallProgress, areAllPuzzlesComplete, //New Progress Tracker
     
-
+    IsPuzzleSolved, CreateBikeLockPuzzle //Create Bike Lock Puzzles
 }
